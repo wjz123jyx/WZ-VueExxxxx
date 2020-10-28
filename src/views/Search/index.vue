b8faf4388f70bd3bb7fa9d8f377f2b05d3f119da
wzz <1393117041@qq.com>
2020-10-28 20:56:31 +0800

    ''

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         <h1>Search</h1>
+        <div style="font-size:50px;color:red;">{{$route.query.keyword2}}</div>
+        <div>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+            <span style="font-size:25px;color:orange;">{{$route.params.keyword}}</span>
+        </div>
     </div>
 </template>
 
